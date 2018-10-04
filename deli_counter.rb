2a8b3930ead3ccf397326416f52bf9e14231d617
Austin Puckett <puckett_austin@yahoo.com>
2018-10-04 05:12:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,10 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
+    message = "The line is currently:"
+    
     array.each_with_index do |val, index|
-      puts "The line is currently: #{index.to_i + 1}. #{val}"
+      message += " #{index.to_i + 1}. #{val}"
     end
   end
 end
